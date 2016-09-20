0839831fa05df1bddc15fe5b93dcb6bccdb48746
Dion <Dion.Fulwood@gmail.com>
2016-09-20 13:03:29 -0700

    Creates post request

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
+'use strict'
 const express = require('express');
 const bodyParser = require('body-parser');
-const secret  = require("./private.js")
-const sendgrid = require('sendgrid')(secret.apiKey);
+const secret  = require('./private.js')
+const sendgrid = require('sendgrid')(secret.apiKey || process.env.apiKey);
+const helper = require('sendgrid').mail;
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
+
+//Create post request
+app.post("/email", (res, req) => {
+  //Store info from user to be used for the e-mail
+  let fromEmail = new.helper.Email("Dion.Fulwood@gmail.com");
+  let toEmail = new.helper.Email(req.body.to);
+  let subject = req.body.subject || "Hello from " + fromEmail.email;
+  let content = new helper.Content("text/plain", req.body.body);
+  //Pass data into Mail constructor
+  let mail = new helper.Mail(fromEmail, subject, toEmail, content);
+
+
+});
 
 
 
