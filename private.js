1aeca8fb632a8d7588494807f5e5511d5afa2639
Dion <Dion.Fulwood@gmail.com>
2016-09-20 12:34:35 -0700

    Configures private.js for api key. Initializes setup of server

---
+++ private.js
@@ -0,0 +1,3 @@
+module.exports = {
+  apiKey: "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"
+}
--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const secret  = require("./private.js")
+const sendgrid = require('sendgrid')(secret.apiKey);
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+
+
+module.exports = app.listen(port, (err)=> {
+  if(err) {
+    console.log(" ❌ You have recieved the following error: ", err);
+  }
+  console.log(`🌎 🌍 🌏 Server is listening on port: ${port} 🌏 🌍 🌎`);
+});
